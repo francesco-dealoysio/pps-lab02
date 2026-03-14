b7dcee4d6609ee6a12a285ecaf3f770d628800fb
Francesco de Aloysio <francesco.dealoysio@studio.unibo.it>
2026-03-14 12:02:13 +0100

    Terminato test 1

--- src/main/scala/it/unibo/pps/u02/Lab2.scala
+++ src/main/scala/it/unibo/pps/u02/Lab2.scala
@@ -1,21 +1,62 @@
 package it.unibo.pps.u02
 
 object Lab2 extends App {
+
+  // Task 1 (warm up), svolto da solo
+
+  println("\n------ Test: println() ")
   println("Hello, Scala")
 
-  // (Int, Int): input
-  // => Int: tipo restituito
+  // multiplication
+  def mult(x: Double, y: Double): Double = x * y
+  def curriedMult(x: Double)(y: Double): Double = x * y
+  def curriedMult3(x: Double)(y: Double)(z: Double): Double = x * y * z
 
-  var f1: (Int, Int) => Int = (a: Int, b: Int) => a + b
+  val m1 = mult(3, 4)
+  val m2 = curriedMult(3)(4)
+  val m3 = curriedMult3(3)(4)(5)
 
-  var f2 = (a: Int, b: Int) => a + b
+  println("\n------ Test: multiplication ")
+  println(mult(-100, -2))
+  println(m1)
+  println(m2)
+  println(m3)
 
-  val f4: (Int, Int) => Int = _ + _
+  // curriedMult redefinition
+  def curriedMult (x: Double): Double = x * 3
+  val multiplyBy3 = curriedMult(6)
+  println(multiplyBy3)
 
-  val f5: (Int, Int) => Int = f4
+  // division
+  def div(x: Double, y: Double): Double = x / y
+  def curriedDiv(x: Double)(y: Double): Double = x / y
 
-  println(s"${f1(10, 5)}, ${f4(10, 5)}")
+  println("\n------ Test: division ")
+  println(div(3,4)) // 0.75
+  println(curriedDiv(2.2)(8)) // 0.275
+  println(curriedDiv(0)(8)) // 0
+  println(curriedDiv(0)(0)) // NaN
+  println(curriedDiv(3)(0)) // Infinity
 
-  var somma = f5(4, 3)
-  println(somma)
+  // Task 2 (functions), svolto da solo
+  val positive: Int => String = _ match
+      case x if x < 0 => "negative"
+      case _ => "positive"
+
+  println("\n------ Test: function with match-case ")
+  println(positive(2))
+  println(positive(-20))
+  println(positive(0))
+  // println(positive(1.2)) // raises an exception
+
+
+  // Task 3 (recursion), svolto da solo
+
+
+  // Task 4 (sum types, product types, modules), svolto da solo
+
+
+  // Task 5 (more functional combinators), svolto da solo
+
+
 }
